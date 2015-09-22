eaf7ebd8e229d929cd286ec16dd9064d8dd6c135
Jason Frame <jason@onehackoranother.com>
2015-09-22 11:25:47 +0100

    tabs -> spaces

--- src/task-list.inc.c
+++ src/task-list.inc.c
@@ -1,35 +1,35 @@
 void task_list_insert_before(struct minitask_list *lst, minitask_task_t *task, minitask_task_t *before) {
-	if (lst->count == 0) {
-		assert(before == NULL);
-		task->next = task;
-		task->prev = task;
-		lst->head = task;
-		lst->count = 1;
-	} else {
-		task->next = before;
-		task->prev = before->prev;
-		before->prev->next = task;
-		before->prev = task;
-		lst->count++;
-	}
+    if (lst->count == 0) {
+        assert(before == NULL);
+        task->next = task;
+        task->prev = task;
+        lst->head = task;
+        lst->count = 1;
+    } else {
+        task->next = before;
+        task->prev = before->prev;
+        before->prev->next = task;
+        before->prev = task;
+        lst->count++;
+    }
 }
 
 void task_list_append(struct minitask_list *lst, minitask_task_t *task) {
-	task_list_insert_before(lst, task, lst->head);
+    task_list_insert_before(lst, task, lst->head);
 }
 
 void task_list_remove(struct minitask_list *lst, minitask_task_t *task) {
-	if (lst->count == 1) {
-		assert(task->next == task);
-		assert(task->prev == task);
-		lst->count = 0;
-		lst->head = NULL;
-	} else {
-		task->prev->next = task->next;
-		task->next->prev = task->prev;
-		if (lst->head == task) {
-			lst->head = task->next;
-		}
-		lst->count--;
-	}
+    if (lst->count == 1) {
+        assert(task->next == task);
+        assert(task->prev == task);
+        lst->count = 0;
+        lst->head = NULL;
+    } else {
+        task->prev->next = task->next;
+        task->next->prev = task->prev;
+        if (lst->head == task) {
+            lst->head = task->next;
+        }
+        lst->count--;
+    }
 }
--- src/types.inc.c
+++ src/types.inc.c
@@ -10,33 +10,33 @@ typedef struct minitask_task_t minitask_task_t;
 typedef void (minitask_entry_f)(void*);
 
 struct minitask_list {
-	int count;
-	minitask_task_t *head;
+    int count;
+    minitask_task_t *head;
 };
 
 struct minitask_scheduler_t {
-	int next_task_id;
-	minitask_task_t *active_task;
-	struct minitask_list active;
-	struct minitask_list waiting;
-	struct {
-		int pending;
-		pthread_mutex_t lock;
-		pthread_cond_t signal;
-		minitask_task_t *head;
-	} wake;
+    int next_task_id;
+    minitask_task_t *active_task;
+    struct minitask_list active;
+    struct minitask_list waiting;
+    struct {
+        int pending;
+        pthread_mutex_t lock;
+        pthread_cond_t signal;
+        minitask_task_t *head;
+    } wake;
 };
 
 struct minitask_task_t {
-	int 					id;
-	void 					*stack;
-	minitask_entry_f 		*entry;
-	void 					*arg;
-	ucontext_t				ctx;
-	minitask_scheduler_t 	*scheduler;
-	minitask_task_t 		*prev;
-	minitask_task_t 		*next;
-	minitask_task_t 		*next_woken;
+    int                     id;
+    void                    *stack;
+    minitask_entry_f        *entry;
+    void                    *arg;
+    ucontext_t              ctx;
+    minitask_scheduler_t    *scheduler;
+    minitask_task_t         *prev;
+    minitask_task_t         *next;
+    minitask_task_t         *next_woken;
 };
 
 void timer_notify_ms(minitask_task_t*, int);
